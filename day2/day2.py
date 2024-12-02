1fb4aad8418b633ae5c4b0e2b69d68f0984e5faf
Justin Schuster <schujustin@gmail.com>
2024-12-02 17:48:12 -0500

    Finished day2 part two

--- day2/day2.py
+++ day2/day2.py
@@ -6,32 +6,65 @@ def readInput():
             out.append(split_line)
     return out
 
+def check_diff(report):
+    for i in range(1, len(report)):
+        diff = abs(int(report[i]) - int(report[i-1]))
+        if diff < 1 or 3 < diff:
+            return False
+    return True
+
+def check_asc_or_desc(report):
+    asc = 0
+    desc = 0
+    n = len(report)
+    for i in range(1, n):
+        if (int(report[i]) - int(report[i-1])) > 0:
+            asc += 1
+
+    for i in range(1, n):
+        if (int(report[i]) - int(report[i-1])) < 0:
+            desc += 1
+
+    if (n-1 == asc) or (n-1 == desc):
+        return True
+    else:
+        return False
+
+def check_remove_one(report):
+    for i in range(1, len(report)):
+        report_copy = report.copy()
+        report_copy.pop(i)
+        diff = check_diff(report_copy)
+        asc_or_desc = check_asc_or_desc(report_copy)
+        if diff and asc_or_desc:
+            return True
+    return False
+
 def part_one():
     out = 0
     reports = readInput()
     for report in reports:
-        safe = True
-        increasing = None
-        n = len(report)
-        for i in range(1, n):
-            diff = int(report[i]) - int(report[i-1]) 
-            if 0 < diff and increasing != False:
-                if increasing == None:
-                    increasing = True
-                if (diff < 1) or (3 < diff):
-                    safe = False
-            elif diff < 0 and increasing != True:
-                if increasing == None:
-                    increasing = False
-                if (-1 < diff) or (diff < -3):
-                    safe = False
-            else:
-                safe = False
-        if safe:
+        diff = check_diff(report)
+        asc_or_desc = check_asc_or_desc(report) 
+        if diff and asc_or_desc:
             out += 1
+    return out 
 
+def part_two():
+    out = 0
+    reports = readInput()
+    for report in reports:
+        diff = check_diff(report)
+        asc_or_desc = check_asc_or_desc(report) 
+        if diff and asc_or_desc:
+            out += 1
+        elif check_remove_one(report):
+            out += 1
     return out
 
 if __name__ == '__main__':
     p1_ans = part_one()
     print(f"Part One: {p1_ans}")
+
+    p2_ans = part_two()
+    print(f"Part Two: {p2_ans}")
