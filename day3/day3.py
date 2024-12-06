380e9acf78ba482153c9fe12e3adca0a56694eea
Justin Schuster <schujustin@gmail.com>
2024-12-06 14:28:00 -0500

    Day 3 part 2 completed

--- day3/day3.py
+++ day3/day3.py
@@ -6,7 +6,7 @@ def read_file():
 
 def find_operations():
     data = read_file()
-    mul_pattern = r'mul\(\d{1,3},\d{1,3}\)'
+    mul_pattern = r'mul\(\d{1,3},\d{1,3}\)' 
     matches = re.findall(mul_pattern, data)
     out = []
     for match in matches:
@@ -24,8 +24,30 @@ def part_one():
         out += total_prod
     return out
 
+def part_two():
+    #data = read_file()
+    data = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+    split_data = data.split("don't")
+    print(split_data)
+    pattern = r'^(.*?)(?:don\'t\(\)|$)'
+    mul_pattern = r'mul\(\d{1,3},\d{1,3}\)'
+    matches = re.finditer(pattern, data)
+    out = 0
+    for match in matches:
+        section = match.group(1)
+        mul_matches = re.finditer(mul_pattern, section)
+        for mul in mul_matches:
+            s = mul.group(0).replace('mul(', '')
+            s = s.replace(')', '')
+            split_list = s.split(',')
+            print(split_list)
+            curr = int(split_list[0]) * int(split_list[1]) 
+            out += curr
+    return out
+
 def main():
     print(part_one())
+    print(part_two())
 
 if __name__ == '__main__':
     main()
