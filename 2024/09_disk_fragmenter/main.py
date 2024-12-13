87a77d9f2fdda52711eeae971facf3a5fdc17633
Justin Schuster <schujustin@gmail.com>
2024-12-13 12:02:15 -0500

    completed part 1 of day 9

--- 2024/09_disk_fragmenter/main.py
+++ 2024/09_disk_fragmenter/main.py
@@ -1,38 +1,31 @@
 def read_input(input='input.txt'):
-    text = open(input, 'r').read()
-    text = '2333133121414131402'
-
-    # text = '12345'
+    text = open(input, 'r').read().replace('\n', '')
     return list(int(digit) for digit in text)
 
 
-def generate_diskmap(input):
-    disk_map = ''
-    file_id = 0
-    for i, digit in enumerate(input):
-        if i == 0 or i % 2 == 0:
-            to_map = str(file_id) * digit
-            disk_map = disk_map + to_map
-            file_id += 1
-        else:
-            to_map = '.' * digit
-            disk_map = disk_map + to_map
-    return disk_map
-
-
-def sort_diskmap(diskmap):
-    dots = 0
-    for i, digit in enumerate(diskmap):
-        if digit == '.':
-            dots += 1
-            for j in diskmap[::-1]:
-                continue
-
-
 def solve(input):
-    diskmap = generate_diskmap(input)
+    files, space = input[::2], input[1::2]
 
-    return diskmap
+    if len(space) < len(files):
+        space.append(1)
+
+    diskmap = []
+    for i, file in enumerate(files):
+        diskmap.extend([i]*file)
+        diskmap.extend([None]*space[i])
+
+    pointer = 0
+    while pointer < len(diskmap):
+        while diskmap[-1] == None:
+            diskmap.pop(-1)
+            pointer -= 1
+
+        if diskmap[pointer] == None:
+            diskmap[pointer] = diskmap.pop(-1)
+
+        pointer += 1
+
+    return sum([x * i for i, x in enumerate(diskmap)])
 
 
 if __name__ == '__main__':
